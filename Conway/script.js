4268b82fa75ce41b0950f02436dcd4ea23fc7a9d
denzilbilson <denzilebilson@gmail.com>
2021-01-06 08:33:14 -0700

    incomplete functions, at checkpoint

--- Conway/script.js
+++ Conway/script.js
@@ -5,6 +5,9 @@ var canvasContext;
 // cells array
 var cells = [];
 
+// grid array
+var grid = [];
+
 window.onload = () => {
     // define how many times setInterval function runs a second
     var framesPerSecond = 10;
@@ -13,6 +16,7 @@ window.onload = () => {
     document.addEventListener("mousedown", createCell);
     canvas = document.getElementById("gameCanvas");
     canvasContext = canvas.getContext('2d');
+    createGrid();
 
     const intervalID = setInterval(() => {
         render();
@@ -26,11 +30,100 @@ function render() {
     })
 }
 
+function createGrid() {
+    for (var i = 0; i < canvas.width / 10; i++) {
+        for (var j = 0; j < canvas.height / 10; j++) {
+            grid.push([i * 10, j * 10, "dead"]);
+        }
+    }
+}
+
+function startGame() {
+    setInterval(() => {
+        birthCell();
+        removeCells();
+        console.log(cells);
+    }, 1000 / 1);
+}
+function birthCell() {
+    var checkCell = JSON.stringify(cells);
+    cells.forEach((coor) => {
+        var sideCells = 0;
+        if (checkCell.includes([coor[0] - 10, coor[1]] - 10)) {
+            sideCells++;
+        }
+        if (checkCell.includes([coor[0], coor[1]] - 10)) {
+            sideCells++;
+        }
+        if (checkCell.includes([coor[0] + 10, coor[1]] - 10)) {
+            sideCells++;
+        }
+        if (checkCell.includes([coor[0] - 10, coor[1]])) {
+            sideCells++;
+        }
+        if (checkCell.includes([coor[0] + 10, coor[1]])) {
+            sideCells++;
+        }
+        if (checkCell.includes([coor[0] - 10, coor[1]] + 10)) {
+            sideCells++;
+        }
+        if (checkCell.includes([coor[0], coor[1]] + 10)) {
+            sideCells++;
+        }
+        if (checkCell.includes([coor[0] + 10, coor[1]] + 10)) {
+            sideCells++;
+        }
+
+        if (sideCells == 3) {
+            cells.push(coor);
+        }
+    })
+}
+function removeCells() {
+    var checkCell = JSON.stringify(cells);
+    grid.forEach((coor) => {
+        var sideCells = 0;
+        if (checkCell.includes([coor[0] - 10, coor[1]] - 10)) {
+            sideCells++;
+        }
+        if (checkCell.includes([coor[0], coor[1]] - 10)) {
+            sideCells++;
+        }
+        if (checkCell.includes([coor[0] + 10, coor[1]] - 10)) {
+            sideCells++;
+        }
+        if (checkCell.includes([coor[0] - 10, coor[1]])) {
+            sideCells++;
+        }
+        if (checkCell.includes([coor[0] + 10, coor[1]])) {
+            sideCells++;
+        }
+        if (checkCell.includes([coor[0] - 10, coor[1]] + 10)) {
+            sideCells++;
+        }
+        if (checkCell.includes([coor[0], coor[1]] + 10)) {
+            sideCells++;
+        }
+        if (checkCell.includes([coor[0] + 10, coor[1]] + 10)) {
+            sideCells++;
+        }
+
+        // game law
+        if (sideCells < 2 || sideCells > 3) {
+            cells.splice(cells.indexOf(coor), 1);
+        }
+    })
+}
+
+// future function to handle clicking and dragging
+function onMouseDown(event) {
+
+}
 function createCell(event) {
     var checkCell = JSON.stringify(cells);
     var rect = canvas.getBoundingClientRect();
-    var xPos = Math.floor((event.clientX - rect.left)/10) * 10;
-    var yPos = Math.floor((event.clientY - rect.top)/10) * 10;
+    var xPos = Math.floor((event.clientX - rect.left) / 10) * 10;
+    var yPos = Math.floor((event.clientY - rect.top) / 10) * 10;
     cells.push([xPos, yPos]);
 }
 
